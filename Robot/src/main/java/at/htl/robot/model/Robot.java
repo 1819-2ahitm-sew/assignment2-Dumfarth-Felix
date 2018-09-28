6cd0c08d7989645a6af9dd437876577157a5c69b
Dumfarth-Felix <f.dumfarth@students.htl-leonding.ac.at>
2018-09-28 10:45:43 +0200

    Dumfarth Felix, Aufgabe Erledigt.

--- Robot/src/main/java/at/htl/robot/model/Robot.java
+++ Robot/src/main/java/at/htl/robot/model/Robot.java
@@ -1,6 +1,58 @@
 package at.htl.robot.model;
 
 public class Robot {
+    private int x;
+    private int y;
+    private Direction direction = Direction.SOUTH;
+
+    //region Getter and Setter
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public Direction getDirection() {
+        return direction;
+    }
+
+    public void setDirection(Direction direction) {
+        this.direction = direction;
+    }
+    //endregion
 
 
+    public void stepForward() {
+        if (this.direction == Direction.NORTH) {
+            this.y--;
+        } else if (this.direction == Direction.EAST) {
+            this.x++;
+        } else if (this.direction == Direction.SOUTH) {
+            this.y++;
+        } else if (this.direction == Direction.WEST) {
+           this.x--;
+        }
+    }
+
+    public void rotateLeft() {
+        if (this.direction == Direction.NORTH) {
+            this.direction = Direction.WEST;
+        } else if (this.direction == Direction.WEST) {
+            this.direction = Direction.SOUTH;
+        } else if (this.direction == Direction.SOUTH) {
+            this.direction = Direction.EAST;
+        } else if (this.direction == Direction.EAST) {
+            this.direction = Direction.NORTH;
+        }
+    }
 }
